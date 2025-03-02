dde4bed2f81289e85ca2a033c207978f58dfc8b1
JasleenKaurSethi <53913719+JasleenKaurSethi@users.noreply.github.com>
2025-03-02 23:59:40 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "aws" {
 }
 
 # module for creating a website using an s3 bucket
-module "website-s3-bucket" {
+module "static-website" {
   source  = "app.terraform.io/Firsts/s3-static-website/s3"
   version = "1.0.0"
   config = {
