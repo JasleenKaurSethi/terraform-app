173cab824b595b07eefd07983974986ad785e94d
JasleenKaurSethi <53913719+JasleenKaurSethi@users.noreply.github.com>
2025-03-03 00:12:23 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
 output "website_bucket_arn" {
   description = "The ARN of the s3 bucket"
-  value       = module.website-s3-bucket.bucket_arn
+  value       = module.static-website.bucket_arn
 }
 
 output "website_bucket_name" {
   description = "The name of the s3 bucket"
-  value       = module.website-s3-bucket.bucket_name
+  value       = module.static-website.bucket_name
 }
 
 output "website_bucket_domain" {
   description = "Domain name for the bucket"
-  value       = module.website-s3-bucket.bucket_domain
+  value       = module.static-website.bucket_domain
 }
 
 output "website_bucket_tags" {
   description = "Tags applied to the bucket"
-  value       = module.website-s3-bucket.bucket_tags
+  value       = module.static-website.bucket_tags
 }
 
 output "website_url" {
   description = "The endpoint URL for the static website on S3"
-  value       = "http://${module.website-s3-bucket.website_endpoint}"
+  value       = "http://${module.static-website.website_endpoint}"
 }
 
